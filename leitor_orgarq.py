8112328f49ff37b714f905067d8093777a569247
Henrique Braz <henrique.braz@acad.pucrs.br>
2019-06-02 23:24:06 -0300

    implementado método para interpretação do exercicio 2

--- leitor_orgarq.py
+++ leitor_orgarq.py
@@ -7,14 +7,15 @@ Created on Thu May 30 23:29:42 2019
 """
 
 
-def hexa_binario(arquivo,hexa_binario):
+def hexa_binario(arquivo_hexa,hexa_binario):
         """
         Método que lê o arquivo inicial com os dados em hexa
-        param arquivo: arquivo inicial a ser lido
-        :return: arquivo hexa_binario.txt com os dados do arquivo em binario
+        param arquivo_hexa: caminho do arquivo inicial com os numeros em hexa a ser lido
+        param hexa_binario: caminho do arquivo onde será salvo os dados no formato  hexa -> binario
+        :return: arquivo binario.txt com os dados formatados e em binario
         """
         lista_binario = []
-        arq = open(arquivo, 'r')
+        arq = open(arquivo_hexa, 'r')
         arq2 = open(hexa_binario,'w')
         arquivo = arq.readlines()
         a = []
@@ -31,15 +32,16 @@ def hexa_binario(arquivo,hexa_binario):
         arq2.close()
         return(lista_binario)
         
-def cria_cash(numeros):
+def cria_cash1(numeros,zeros):
     """
     Método que cria uma lista de listas, com a pósição inicial de cada linha da cash
     param numero: quantidade total de posições da cash, em decimal
+    param zeros: quantidade de casas da linha (ex. 3 -> 000, 4 -> 0000)
     """
     cash = [[] for _ in range(numeros)]
     for i in range(numeros):
         b = hex(i)
-        cash[i].append(bin(int(b, 16))[2:].zfill(4)) #linha indice: 0
+        cash[i].append(bin(int(b, 16))[2:].zfill(zeros)) #linha indice: 0
         cash[i].append('') #v_cash    indice: 1
         cash[i].append('') #tag_cash  indice: 2
         cash[i].append('') #palavra0  indice: 3
@@ -52,6 +54,27 @@ def cria_cash(numeros):
         cash[i].append('') #palavra7  indice: 10
         cash[i].append('') #b_byte    indice: 11
         cash[i].append('') #Miss/Hit  indice: 12
+            
+    return(cash)
+    
+def cria_cash2(numeros,zeros):
+    """
+    Método que cria uma lista de listas, com a pósição inicial de cada linha da cash
+    param numero: quantidade total de posições da cash, em decimal
+    param zeros: quantidade de casas da linha (ex. 3 -> 000, 4 -> 0000)
+    """
+    cash = [[] for _ in range(numeros)]
+    for i in range(numeros):
+        b = hex(i)
+        cash[i].append(bin(int(b, 16))[2:].zfill(zeros)) #linha indice: 0
+        cash[i].append('') #v_cash    indice: 1
+        cash[i].append('') #tag_cash  indice: 2
+        cash[i].append('') #palavra0  indice: 3
+        cash[i].append('') #palavra1  indice: 4
+        cash[i].append('') #palavra2  indice: 5
+        cash[i].append('') #palavra3  indice: 6
+        cash[i].append('') #b_byte    indice: 7
+        cash[i].append('') #Miss/Hit  indice: 8
             
     return(cash)
     
@@ -70,23 +93,21 @@ def organiza_palavra(palavra,ultimos_bits):
         b = (hex(i))
         c = (bin(int(b, 16))[2:].zfill(ultimos_bits))
         lista_ultimos_bits.append(y+c)
-        
+            
     return(lista_ultimos_bits)
-        
-    
-    
+         
         
         
 def exercio1(lista_binario,exercicio1):
     """
     Método que faz a análise da cash 
     :param lista_binario: lista convertida de hexa para binario 
-    :param arquivo_pronto: caminho do arquivo.txt onde será salvo os dados da análise
+    :param exercicio1: caminho do arquivo.txt onde será salvo os dados da análise
     utiliza do método organiza_palavra para inserir os ultimos bits nas posições do bloco
-    utiliza do método cria_cash para criar a cash vazia
+    utiliza do método cria_cash para criar a cash vazia, com 16 libhas e 4 casas
     """
     cash = []
-    cash = cria_cash(16) #cria a cash com 16 linhas
+    cash = cria_cash1(16,4) #cria a cash com 16 linhas
     indice = ''
     hit = 0
     miss = 0
@@ -126,8 +147,8 @@ def exercio1(lista_binario,exercicio1):
             hit += 1
             
            
-        print('cash[indice]:')    
-        print(cash[indice])
+        #print('cash[indice]:')    
+        #print(cash[indice])
         arq.write('\n'+str(cash[indice])+'\n')
         
     arq.write('\nCash, resultado final: \n')
@@ -137,7 +158,65 @@ def exercio1(lista_binario,exercicio1):
     arq.write(str(hit) +'\n')
     arq.write('Total de Cash Miss:\n')
     arq.write(str(miss))
-    arq.close()            
+    arq.close()
+
+def exercio2(lista_binario,exercicio2):
+    """
+    Método que faz a análise da cash 
+    :param lista_binario: lista convertida de hexa para binario 
+    :param exercicio2: caminho do arquivo.txt onde será salvo os dados da análise
+    utiliza do método organiza_palavra para inserir os ultimos bits nas posições do bloco
+    utiliza do método cria_cash para criar a cash vazia
+    """
+    cash = []
+    cash = cria_cash2(32,5) #cria a cash com 32
+    indice = ''
+    hit = 0
+    miss = 0
+    arq = open(exercicio2,'w')
+    arq.write('\nlinha, bit_validade, tag, palavra0, palavra1, palavra2, palavra3, bit para byte, Hit/Miss\n')
+    for i in range(len(lista_binario)):
+        x = str(lista_binario[i])
+        tag = x[0:8]
+        linha  = x[8:13]
+        palavra = x[0:15]
+        b_byte = x[15]
+        for j in range(len(cash)):
+            
+            if linha in cash[j]:
+                indice = j #achei a linha: cash[indice] 
+            
+        if cash[indice][2] != tag:
+            
+            cash[indice][1] = '1'
+            cash[indice][2] = tag
+            cash[indice][3] = organiza_palavra(palavra,2)[0]
+            cash[indice][4] = organiza_palavra(palavra,2)[1]
+            cash[indice][5] = organiza_palavra(palavra,2)[2]
+            cash[indice][6] = organiza_palavra(palavra,2)[3]
+            cash[indice][7] = b_byte
+            cash[indice][8] = 'Miss'
+            miss += 1
+       
+        elif cash[indice][2] == tag:
+            
+            cash[indice][7] = b_byte
+            cash[indice][8] = 'Hit'
+            hit += 1
+            
+           
+        #print('cash[indice]:')    
+        #print(cash[indice])
+        arq.write('\n'+str(cash[indice])+'\n')
+        
+    arq.write('\nCash, resultado final: \n')
+    arq.write('\nlinha, bit_validade, tag, palavra0, palavra1, palavra2, palavra3, bit para byte, Hit/Miss\n')     
+    arq.write('\n'+str(cash)+'\n')    
+    arq.write('\nTotal de Cash Hit:\n')
+    arq.write(str(hit) +'\n')
+    arq.write('Total de Cash Miss:\n')
+    arq.write(str(miss))
+    arq.close()             
             
     
     
@@ -145,12 +224,19 @@ def exercio1(lista_binario,exercicio1):
 
 if __name__ == "__main__":
     
-    lista_binario = hexa_binario('/home/henrique/orgarqt3/orgarqT3/arquivo','/home/henrique/orgarqt3/orgarqT3/hexa_binario')    
-    
+    lista_binario = hexa_binario('/home/henrique/orgarqt3/orgarqT3/arquivo_hexa','/home/henrique/orgarqt3/orgarqT3/hexa_binario')    
+
     exercio1(lista_binario,'/home/henrique/orgarqt3/orgarqT3/exercicio1')
     
+    exercio2(lista_binario,'/home/henrique/orgarqt3/orgarqT3/exercicio2')
     
     
+    #Mapeamento direto, com 8 bits para tag, 4 bits para linha, 3 bits
+    #para palavra e 1 bit para byte (cache com 16 linhas, 8 palavras por linha).
+    
+    
+    #Mapeamento direto, com 8 bits para tag, 5 bits para linha, 2 bits
+    #para palavra e 1 bit para byte (cache com 32 linhas, 4 palavras por linha).
     
     
     
